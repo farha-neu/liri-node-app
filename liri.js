53b11941689e57305b0899d720c23677e66a338a
Farha B. Jawed <farhajw@gmail.com>
2018-06-08 01:53:43 -0700

    omdb

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-
+var request = require("request");
 
 var keys = require("./keys.js");
 
@@ -57,8 +57,36 @@ else if(command === "spotify-this-song"){
     } 
 }
 else if(command === "movie-this"){
+    if(!track){
+        track = "Mr.Nobody";
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + track + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
 
+
+    request(queryUrl, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        var movieObject = JSON.parse(body);
+        console.log("Title: " + movieObject.Title);
+        console.log("Year: " +  movieObject.Year);
+        console.log("Rotten Tomato Rating: " +  movieObject.Ratings[1].value);
+        console.log("Country Produced: " +  movieObject.Country);
+        console.log("Language: " +  movieObject.Language);
+        console.log("Plot: " +  movieObject.Plot);
+        console.log("Actors: " +  movieObject.Actors);
+    }
+    });
 }
+
+// * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
 
 // my-tweets
 
